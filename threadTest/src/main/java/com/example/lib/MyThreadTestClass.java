9c9135ad08748c1ccb7456f3562d95a396ece333
zipinghuang <huangzp152@163.com>
2020-08-31 00:18:45 +0800

    线程基础应用实例2

--- threadTest/src/main/java/com/example/lib/MyThreadTestClass.java
+++ threadTest/src/main/java/com/example/lib/MyThreadTestClass.java
@@ -33,14 +33,53 @@ public class MyThreadTestClass {
         }
         UseThread useThread = new UseThread();
         useThread.start();
-        useThread.setDaemon(true);
-        UseThread2 useThread22 = new UseThread2();
+//        useThread.setDaemon(true);
+//        UseThread2 useThread22 = new UseThread2();
+
 //        useThread.start();
 
 //        Thread.sleep(10);
 //        useThread.join();
 //        useThread.interrupt();//停止不下来的，这是是发送信号而已
+        MyThreadTestClass myThreadTestClass = new MyThreadTestClass();
+        Count count1 = new Count(myThreadTestClass);
+        count1.start();
+        Count count2 = new Count(myThreadTestClass);
+        count2.start();
+        Thread.sleep(50);
+        System.out.println(myThreadTestClass.count);
 
+    }
+
+    private static class Count extends Thread {
+
+        public MyThreadTestClass myThreadTestClass;
+
+        public Count(MyThreadTestClass myThreadTestClass) {
+            this.myThreadTestClass = myThreadTestClass;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            for (int i = 0; i< 10000; i++) {
+                myThreadTestClass.incCount();
+            }
+        }
+    }
+
+    private long count = 0;
+    //
+    public synchronized void incCount() {
+        count++;
+    }
+
+    //跟#incCount等价
+    //类锁
+    public void incCount2() {
+        synchronized (MyThreadTestClass.this) {
+            count++;
+        }
     }
 
     /**
